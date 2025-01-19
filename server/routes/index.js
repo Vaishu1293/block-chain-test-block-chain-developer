f85b63efdc20225c3a807741a896ebfecef144a1
vaish shivkumar <vaish.1231993.vaishali@gmail.com>
2025-01-19 07:45:18 +0000

    api-integration-phase-one

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,10 +5,12 @@ const userRoute = require("./userRoute");
 const orderRoute = require("./orderRoute");
 const paymentRoute = require("./paymentRoute");
 const productRoute = require("./productRoute");
+const vaishRoute = require("./vaishRoute");
 
 router.route("/user", userRoute);
 router.route("/order", orderRoute);
 router.route("/payment", paymentRoute);
 router.route("/product", productRoute);
+router.route("/vaishapitest", vaishRoute);
 
 module.exports = router;
