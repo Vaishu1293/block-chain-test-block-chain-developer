1502b9584982b924a68792d431a8a2c9c8f2ca76
vaish shivkumar <vaish.1231993.vaishali@gmail.com>
2025-01-19 07:30:30 +0000

    first commit

---
+++ src/utils/tryParseAmount.ts
@@ -0,0 +1,32 @@
+import { parseUnits } from "@ethersproject/units";
+import { JSBI } from "@pancakeswap/sdk";
+import type { Currency } from "@pancakeswap/swap-sdk-core";
+import { CurrencyAmount } from "@pancakeswap/swap-sdk-core";
+
+// try to parse a user entered amount for a given token
+function tryParseAmount<T extends Currency>(
+  value?: string,
+  currency?: T
+): CurrencyAmount<T> | undefined {
+  if (!value || !currency) {
+    return undefined;
+  }
+  try {
+    const typedValueParsed = parseUnits(value, currency.decimals).toString();
+
+    if (typedValueParsed !== "0") {
+      return CurrencyAmount.fromRawAmount(
+        currency,
+        JSBI.BigInt(typedValueParsed).toString()
+      );
+    }
+  } catch (error) {
+    // should fail if the user specifies too many decimal places of precision (or maybe exceed max uint?)
+    console.debug(`Failed to parse input amount: "${value}"`, error);
+  }
+  // necessary for all paths to return a value
+  return undefined;
+}
+
+export default tryParseAmount;
+
